975953074c3c239c6186fa288881db2528c3aab4
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-18 15:09:54 -0400

    Added Scaffold for topAppBar

--- app/src/main/java/com/codingchallenge/fetchrewardscodingchallenge/presentation/home_screen/ItemListScreen.kt
+++ app/src/main/java/com/codingchallenge/fetchrewardscodingchallenge/presentation/home_screen/ItemListScreen.kt
@@ -3,9 +3,7 @@ package com.codingchallenge.fetchrewardscodingchallenge.presentation.home_screen
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,54 +21,73 @@ import com.codingchallenge.fetchrewardscodingchallenge.presentation.components.I
 fun ItemListScreen(
     viewModel: ItemListScreenViewModel = hiltViewModel()
 ) {
-    when(viewModel.state.isLoading){
-        true -> {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                CircularProgressIndicator()
-            }
+    Scaffold(
+        modifier = Modifier
+            .padding(0.dp, 0.dp, 0.dp, 16.dp),
 
-        }
-        else -> {
-            LazyColumn(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            ){
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center) {
+                        Text("Coding Challenge", textAlign = TextAlign.Center, color = Color.White)
 
-                items(viewModel.state.originalListItems.keys.toList()) { key->
-                    Row(modifier = Modifier
-                        .padding(0.dp, 16.dp, 0.dp, 0.dp)
-                        .fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            text = "ListID: ${key.toString()}",
-                            textAlign = TextAlign.Center,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Bold
-                        )
                     }
-                    Row {
-                        Divider(
-                            modifier = Modifier
-                                .padding(0.dp, 0.dp, 0.dp, 16.dp),
-                            color = Color.Black,
-                            thickness = 2.dp
-                        )
-                    }
+                }
+            )
 
-
-                    viewModel.state.originalListItems[key]?.forEach { ItemCard(Item = it) }
-
+        }) {
+        when(viewModel.state.isLoading){
+            true -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    CircularProgressIndicator()
                 }
 
             }
+            else -> {
+                LazyColumn(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ){
+
+                    items(viewModel.state.originalListItems.keys.toList()) { key->
+                        Row(modifier = Modifier
+                            .padding(0.dp, 16.dp, 0.dp, 0.dp)
+                            .fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "ListID: ${key.toString()}",
+                                textAlign = TextAlign.Center,
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Row {
+                            Divider(
+                                modifier = Modifier
+                                    .padding(0.dp, 0.dp, 0.dp, 16.dp),
+                                color = Color.Black,
+                                thickness = 2.dp
+                            )
+                        }
+
+                        viewModel.state.originalListItems[key]?.forEach { ItemCard(Item = it) }
+
+                    }
+
+                }
+            }
         }
+        
     }
+
 
 }
 
