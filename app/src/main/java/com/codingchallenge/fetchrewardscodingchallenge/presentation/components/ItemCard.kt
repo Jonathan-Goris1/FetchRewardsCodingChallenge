672d2c92738b9c7ebd0c86bc9a3fcd1100c92547
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-18 18:43:21 -0400

    Added testTags to all the text for testing purposes

--- app/src/main/java/com/codingchallenge/fetchrewardscodingchallenge/presentation/components/ItemCard.kt
+++ app/src/main/java/com/codingchallenge/fetchrewardscodingchallenge/presentation/components/ItemCard.kt
@@ -7,11 +7,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.codingchallenge.fetchrewardscodingchallenge.data.remote.model.ItemListModelItem
+import com.codingchallenge.fetchrewardscodingchallenge.utils.TestTags
 
 @Composable
 fun ItemCard(
@@ -30,6 +32,7 @@ fun ItemCard(
         ) {
             Text(
                 modifier = modifier
+                    .testTag(TestTags.ID_Text)
                     .padding(0.dp,0.dp,16.dp,0.dp)
                     .weight(1.0f),
                 textAlign = TextAlign.Left,
@@ -39,15 +42,18 @@ fun ItemCard(
 
 
             Text(
-                modifier = modifier.
-                padding(0.dp,0.dp,16.dp,0.dp)
+                modifier = modifier
+                    .testTag(TestTags.ListID_Text)
+                    .padding(0.dp,0.dp,16.dp,0.dp)
                     .weight(1.0f),
                 fontSize = 16.sp,
                 text = "listID: ${Item.listId.toString()}"
             )
 
             Text(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier
+                    .testTag(TestTags.Name_Text)
+                    .weight(1f),
                 fontSize = 16.sp,
                 text = "Name: ${Item.name?: ""}"
             )
