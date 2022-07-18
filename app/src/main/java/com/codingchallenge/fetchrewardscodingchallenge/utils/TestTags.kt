12cf8a3df9ab0f72bae91bc8602d2561dda3846b
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-18 18:44:40 -0400

    Created object for TestTag names

---
+++ app/src/main/java/com/codingchallenge/fetchrewardscodingchallenge/utils/TestTags.kt
@@ -0,0 +1,10 @@
+package com.codingchallenge.fetchrewardscodingchallenge.utils
+
+object TestTags {
+    //Home Screen
+    const val GroupID_text = "groupId_text"
+    const val ID_Text = "id_text"
+    const val ListID_Text = "listId_text"
+    const val Name_Text = "name_text"
+
+}
